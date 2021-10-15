cf8e956f47f848765f43a9328eab55b6539db66b
César <cesar.tailleur@oxatis.com>
2021-10-15 23:56:48 +0200

    ReactFlowComponent: removed useless constructor method

--- src/pages/ReactFlowComponent.js
+++ src/pages/ReactFlowComponent.js
@@ -2,10 +2,6 @@ import React from "react";
 import ReactFlow from "react-flow-renderer";
 
 export class ReactFlowComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div style={{ height: 600 }}>
